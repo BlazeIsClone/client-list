167566921a915656cb63431b23974f0055a749db
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-07-03 15:01:53 +0530

    config: remove styleci

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Helpers\RouteHelper;
-use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->middleware('cors')->group(function () {
-    RouteHelper::class::includeRouteFiles(__DIR__.'/v1');
+    RouteHelper::class::includeRouteFiles(__DIR__ . '/v1');
 });
