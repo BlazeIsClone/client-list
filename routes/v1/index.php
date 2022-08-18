d7b4ad0a8aedf83a406db8ac3b8463faadcbae34
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-08-18 10:45:45 +0530

    remove index route listing

--- routes/v1/index.php
+++ routes/v1/index.php
@@ -24,11 +24,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('clients', ClientController::class);
 Route::resource('companies', CompanyController::class);
-
-Route::get('/', function () {
-    foreach (Route::getRoutes() as $value) {
-        $data[] = $value;
-    }
-
-    return new JsonResource($data);
-});
